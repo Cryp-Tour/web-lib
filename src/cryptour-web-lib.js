271386518244b28fde12c941e5cb6faf4a67ed96
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2022-02-12 15:51:47 +0100

    Add Init Wallet

--- src/cryptour-web-lib.js
+++ src/cryptour-web-lib.js
@@ -1,17 +1,20 @@
+console.log("[CrypTourWeb] Using Cryp-Tour Web Lib: https://github.com/Cryp-Tour");
 CrypTourWeb = {
+    accounts: null,
+    debug: false,
     web3provider: null,
     contracts: {},
-    listeners: { 
+    listeners: {
         onload: []
     },
-    contractJSONPaths: ["/contracts/BPool.json"],
+    contractJSONPaths: ["contracts/BPool.json"],
     state: {
         initWeb3: false,
         initContracts: 0
     },
 
     addOnLoadListener: function (callback) {
-        onload.push(callback);
+        CrypTourWeb.listeners.onload.push(callback);
     },
 
     init: async function () {
@@ -19,12 +22,18 @@ CrypTourWeb = {
     },
 
     initWeb3: function () {
+        if (CrypTourWeb.debug)
+            console.log("[CrypTourWeb] Init Web3")
         // Modern dapp browsers...
         if (window.ethereum) {
+            if (CrypTourWeb.debug)
+                console.log("[CrypTourWeb] Init Web3 as window.ethereum")
             CrypTourWeb.web3Provider = window.ethereum;
         }
         // Legacy dapp browsers...
         else if (window.web3) {
+            if (CrypTourWeb.debug)
+                console.log("[CrypTourWeb] Init Web3 as window.web3")
             CrypTourWeb.web3Provider = window.web3.currentProvider;
         }
         // TODO: fallback for not compatible Browsers
@@ -34,28 +43,75 @@ CrypTourWeb = {
 
     initContract: function () {
         CrypTourWeb.contractJSONPaths.forEach(path => {
+            if (CrypTourWeb.debug)
+                console.log("[CrypTourWeb] Get Contract " + path)
             $.getJSON(path, function (data) {
                 // Get the necessary contract artifact file and instantiate it with @truffle/contract
                 let ContractArtifact = data;
                 let name = ContractArtifact.contractName;
 
                 CrypTourWeb.contracts[name] = TruffleContract(ContractArtifact);
-        
+
                 // Set the provider for our contract
                 CrypTourWeb.contracts[name].setProvider(CrypTourWeb.web3Provider);
 
                 // Call Listener for Onload 
                 CrypTourWeb.state.initContracts++;
                 if (CrypTourWeb.state.initContracts == CrypTourWeb.contractJSONPaths.length) {
-                    CrypTourWeb.callListeners(CrypTourWeb.listeners.onload, undefined);
+                    CrypTourWeb.__callListeners(CrypTourWeb.listeners.onload, undefined);
+
+                    if (CrypTourWeb.debug)
+                        console.log("[CrypTourWeb] Got all Contracts")
                 }
             });
         })
     },
 
-    callListeners: function (listeners, data) {
+    __callListeners: function (listeners, data) {
         listeners.forEach(listener => {
             listener(data);
+        });
+    },
+
+    // Returns a Promise
+    initWallet: function () {
+        var x = new Promise((resolve, reject) => {
+
+            if (CrypTourWeb.debug)
+                console.log("[CrypTourWeb] Init Wallet")
+            if (window.ethereum) {
+                // Request account access
+                window.ethereum.request({ method: 'eth_requestAccounts' }).then(() => {
+                    CrypTourWeb.__initWallet2(resolve);
+                }).catch((error) => {
+                    // User denied account access...
+                    if (CrypTourWeb.debug)
+                        console.error("User denied account access")
+                    reject(error);
+                })
+            }
+            else
+                CrypTourWeb.__initWallet2(resolve);
+
+        })
+        return x;
+    },
+
+    __initWallet2: function (resolve) {
+        web3 = new Web3(CrypTourWeb.web3Provider);
+        web3.eth.getAccounts(function (error, accounts) {
+            if (error) {
+                if (CrypTourWeb.debug)
+                    console.log(error);
+                reject(error);
+            }
+            CrypTourWeb.accounts = accounts;
+
+            if (CrypTourWeb.debug) {
+                console.log("[CrypTourWeb] Accounts are: ");
+                console.log(accounts);
+            }
+            resolve();
         });
     }
 }
